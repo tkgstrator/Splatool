6a4926bc17164d81fea8ff5acb4c75348af70a84
tkgstrator <nasawake.am@gmail.com>
2022-07-30 20:58:55 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'splatoon',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
