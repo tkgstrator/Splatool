8ba62f786ad9e0c5721aa8b36ebeeedd7fc87c7b
tkgstrator <nasawake.am@gmail.com>
2022-07-31 02:02:16 +0900

    - Use setState method

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
+  IonImg,
   IonLabel,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { informationCircleOutline } from "ionicons/icons";
+import User from "./pages/User";
+import SignIn from "./pages/SignIn";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
@@ -41,31 +41,28 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/user">
+            <User />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route exact path="/setting">
+            <SignIn />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/user" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="user" href="/user">
+            <IonImg
+              src="https://www.nintendo.co.jp/character/splatoon/images_en/common/ika.png"
+              style={{ height: "24.16px" }}
+            ></IonImg>
+            {/* <IonIcon icon={triangle} /> */}
+            <IonLabel>アカウント</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="setting" href="/setting">
+            <IonIcon icon={informationCircleOutline} />
+            <IonLabel>ログイン</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,112 @@
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonToast,
+} from "@ionic/react";
+import { useState } from "react";
+import "./SignIn.css";
+import axios, { AxiosError } from "axios";
+
+interface OAuth {
+  oauthURL: string;
+  session_token_verifier: string;
+}
+
+export interface SplatNet2 {
+  nickname: string;
+  nsaid: string;
+  session_token: string;
+  iksm_session: string;
+  thumbnail_url: string;
+  expires_in: number;
+}
+
+export interface APIError {
+  error: string;
+  error_description: string;
+}
+
+const SignIn: React.FC = () => {
+  const [sessionTokenCode, setCode] = useState<string>();
+  const [sessionTokenCodeVerifier, setVerifier] = useState<string>();
+  const [present, dismiss] = useIonToast();
+
+  async function getOAuthURL() {
+    const url = `${process.env.REACT_APP_SERVER_URL}/authorize`;
+    const response = (await (await fetch(url)).json()) as OAuth;
+    const verifier = response.session_token_verifier;
+    setVerifier(verifier);
+    window.open(response.oauthURL, "_blank");
+  }
+
+  async function getCookie() {
+    try {
+      const url = `${process.env.REACT_APP_SERVER_URL}/login`;
+      const parameters = {
+        session_token_code: sessionTokenCode,
+        session_token_code_verifier: sessionTokenCodeVerifier,
+      };
+      const response = (await axios.post(url, parameters)).data as SplatNet2;
+      localStorage.setItem("account", JSON.stringify(response));
+      present({
+        message: response.nickname + "さんにログインしました",
+        duration: 3000,
+      });
+    } catch (error) {
+      const response = (error as AxiosError).response?.data as APIError;
+      present({
+        message: response.error_description,
+        duration: 3000,
+      });
+    }
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>SignIn</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">SignIn</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonList>
+          <IonItem>
+            <IonLabel slot="start">イカリング2</IonLabel>
+            <IonButton slot="end" onClick={getOAuthURL}>
+              リンクを開く
+            </IonButton>
+          </IonItem>
+          <IonItem>
+            <IonLabel slot="start">URL入力</IonLabel>
+            <IonInput
+              type="password"
+              value={sessionTokenCode}
+              onIonChange={(e) => setCode(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel slot="start">ログイン</IonLabel>
+            <IonButton slot="end" onClick={getCookie}>
+              連携
+            </IonButton>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SignIn;
